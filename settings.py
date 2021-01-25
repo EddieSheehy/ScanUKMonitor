663845fa17091207b74234bbe2eb7fe6af4412ee
EddieSheehy <51293802+EddieSheehy@users.noreply.github.com>
2021-01-25 05:49:03 +0000

    Update settings.py

--- settings.py
+++ settings.py
@@ -2,3 +2,5 @@ MIDDLEWARE = [
   'django.middleware.security.SecurityMiddleware',
   'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
