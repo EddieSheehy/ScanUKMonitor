93e3580c01b7c3532acbd9376ffda737f9891c01
EddieSheehy <51293802+EddieSheehy@users.noreply.github.com>
2021-01-25 05:53:27 +0000

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,13 @@
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
+from my_project import MyWSGIApp
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "py_mrp.settings")
 
 application = get_wsgi_application()
+application = MyWSGIApp()
+application = WhiteNoise(application, root='/path/to/static/files')
+application.add_files('/path/to/more/static/files', prefix='more-files/')
