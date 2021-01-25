0d0f82a590be7b62b24d5de8f8d91b382f7b19e3
EddieSheehy <51293802+EddieSheehy@users.noreply.github.com>
2021-01-25 19:30:40 +0000

    Update Scan_UK.py

--- Scan_UK.py
+++ Scan_UK.py
@@ -5,7 +5,7 @@ import time
 from bs4 import BeautifulSoup
 from threading import Thread
 
-headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+headers = {'User-Agent': 'Mozilla/5.0 (Android 4.4; Mobile; rv:41.0) Gecko/41.0 Firefox/41.0'}
 TOKEN = 'NzExMjU2NjU4NTkyMTM3MjM3.XsAXYQ.RsuGF9pIAtU3dguVz7-EclQRy34'
 client = discord.Client()
 webhookurl = 'https://discord.com/api/webhooks/803110829801209868/EIfQlHKLH69f3-I4EZ2e1_3QxPAFq4K1YRd9a2X9WNYWGur8M3pGYeornKrwfMvnMlSv'
